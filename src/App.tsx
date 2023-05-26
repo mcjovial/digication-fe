b8921e2317e114ef24141cafb5bdd9661bccc502
Emmanuel Elias <emmanuelelias455@gmail.com>
2023-05-26 22:55:10 +0100

    added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import Portfolio from './components/Portfolio';
+import './app.css';
 
 const App = () => {
   return (
     <>
+      <h1 className="text-primary text-center text-2xl font-bold my-10">Full-Stack / Frontend homework (Typescript, React, Drag & Drop)</h1>
       <CssBaseline />
       <Portfolio />
     </>
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+      extend: {
+          colors: {
+              primary: '#1B73E8',
+          },
+      },
+  },
+  plugins: [],
+};
